5dd35e41e6b9eb3710c84c0fd6e1865dcbb59b49
Ravindra Singh <raviss@Ravindras-MacBook-Pro.local>
2021-06-26 13:16:19 +0530

    update bio

--- src/components/bio/Utils.js
+++ src/components/bio/Utils.js
@@ -1,11 +1,11 @@
 export const BIO_MD = "I am a hobbyist software developer and an explorer.\n" +
-  "\n" +
-  "In year 2019, I started by programming journey with C and been in this rabbit hole ever since. During this time I have worked on a range of technologies such as Javascript, Python, Vue, React, Django, MySQL, and the list goes on, and the fundamentals that were built at IIITG, helped me always.\n" +
-  "\n" +
-  "Currently, I am an Intern and exploring the world of large scale software, meeting new people and learning along.\n" +
-  "\n" +
-  "Mostly, I have worked on frontend web technologies such as Vue, React, etc. and I am always intrigued by the scale a frontend architecture can achieve, and I am always ready to learn as the frontend world keeps on enhancing.\n" +
-  "\n" +
-  "I am always looking forward to trying new ideas and work on exciting projects.\n" +
-  "\n" +
-  "I also like to  wander different places, which might be a reason for me choosing IIIT Guwahati. I have travelled some places, including, north-east, pondicherry, chikmangloor, etc, and looking forward to cover more locations in the future :)\n";
+    "\n" +
+    "In year 2019, I started my programming journey with C and been in this rabbit hole ever since. During this time I have worked on a range of technologies such as Javascript, Python, Vue, React, Django, MySQL, and the list goes on, and the fundamentals that were built at IIITG helped me always.\n" +
+    "\n" +
+    "Currently, I am an Intern and exploring the world of large scale softwares, meeting new people and learning along.\n" +
+    "\n" +
+    "Mostly, I have worked with frontend web technologies such as Vue, React, etc. and I am always intrigued by the scale a frontend architecture can achieve, and I am always ready to learn as the frontend world keeps on enhancing.\n" +
+    "\n" +
+    "I am always looking forward to trying new ideas and work on exciting projects.\n" +
+    "\n" +
+    "I also like to  wander different places, which might be a reason for me choosing IIIT Guwahati. I have travelled some places, including, north-east, pondicherry, chikmangloor, etc, and looking forward to cover more locations in the future.\n"
