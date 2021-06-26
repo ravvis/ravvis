eef64207b9290ce54b95a16cf7e9642e8dac0d16
Ravindra Singh <raviss@Ravindras-MacBook-Pro.local>
2021-06-26 14:49:45 +0530

    responsive card header

--- src/components/projects/project/ProjectCard.js
+++ src/components/projects/project/ProjectCard.js
@@ -5,32 +5,36 @@ const ProjectCard = ({ title, content, image_src, image, description, technologi
   return (
     <>
       <Card>
-        <div className="d-flex justify-content-between p-sm-3 my-1">
-          <div>
-            <Link href={url} target="_blank">
-              <CardHeader title={title} subheader={description}/>
-            </Link>
-            <CardContent>
-              {
-                content &&
-                <Typography variant="subtitle2" display="block" gutterBottom>
-                  <ReactMarkdown>{ content }</ReactMarkdown>
-                </Typography>
-              }
-              <Typography variant="caption" display="block" gutterBottom>
-                { time }
+        <div className="p-sm-3 my-1">
+          <Link href={url} target="_blank">
+            <div className="d-flex justify-content-between flex-nowrap">
+              <CardHeader
+                title={title}
+                subheader={description}
+                titleTypographyProps={{ className: "fs-3 pb-1" }}
+              />
+              <div className="p-3 d-none d-sm-block">
+                { !!image && image }
+                { !!image_src && <img src={image_src} width="60px" alt="img"/> }
+              </div>
+            </div>
+          </Link>
+          <CardContent>
+            {
+              content &&
+              <Typography variant="subtitle2" display="block" gutterBottom>
+                <ReactMarkdown>{ content }</ReactMarkdown>
               </Typography>
-              <Typography variant="caption" display="block" gutterBottom>
-                <div className="d-flex">
-                  {technologies.map((tech, index) => <div className="pe-2">{tech}{index !== technologies.length - 1 ? ", " : ""}</div>)}
-                </div>
-              </Typography>
-            </CardContent>
-          </div>
-          <div className="p-3">
-            { !!image && image }
-            { !!image_src && <img src={image_src} width="60px" alt="img"/> }
-          </div>
+            }
+            <Typography variant="caption" display="block" gutterBottom>
+              { time }
+            </Typography>
+            <Typography variant="caption" display="block" gutterBottom>
+              <div className="d-flex">
+                {technologies.map((tech, index) => <div className="pe-2">{tech}{index !== technologies.length - 1 ? ", " : ""}</div>)}
+              </div>
+            </Typography>
+          </CardContent>
         </div>
       </Card>
       <div className="py-2"/>
