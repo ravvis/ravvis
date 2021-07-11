dbf4d7d7bd234f82578ab3048bf1daa6c6e04dae
Ravindra Singh <raviss@Ravindras-MacBook-Pro.local>
2021-07-11 22:37:19 +0530

    Fouc fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,49 @@
 import '../src/index.css';
+import {createMuiTheme, ThemeProvider} from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import React from "react";
+import Head from "next/head";
+import pink from "@material-ui/core/colors/pink";
 
-function MyApp({ Component, pageProps }) {
-  return <>
-    <Component {...pageProps} />
-  </>
+export const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+  palette: {
+    type: "dark",
+    primary: {
+      main: '#4db6ac'
+    },
+    secondary: pink,
+    background: {
+      default: '#0b0e11',
+      paper: '#242c37'
+    }
+  },
+});
+
+export default function MyApp({ Component, pageProps }) {
+  return (<>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Ravindra's personal homepage - Developer / Explorer</title>
+    </Head>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.StrictMode>
+  </>)
 }
-export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,10 @@
-import {createMuiTheme, ThemeProvider} from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import App from "../src/App";
-import React from "react";
-import pink from "@material-ui/core/colors/pink";
 import reportWebVitals from '../src/reportWebVitals';
+import React from "react";
 
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-  },
-  palette: {
-    type: "dark",
-    primary: {
-      main: '#4db6ac'
-    },
-    secondary: pink,
-    background: {
-      default: '#0b0e11',
-      paper: '#242c37'
-    }
-  },
-});
-
-const Index = () => {
-  return <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+export default function Index() {
+  return <App />
 }
-export default Index;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
