9b7002e67b9d0777886be1acc4df21c13138f370
Ravindra Singh <raviss@Ravindras-MacBook-Pro.local>
2021-08-11 13:15:01 +0530

    add opengraph meta tags :D

--- pages/_app.js
+++ pages/_app.js
@@ -21,9 +21,15 @@ export default function MyApp({ Component, pageProps }) {
     setFavColorLocal(getFavColor());
     console.log("Currently selected fav color: ", favColorLocal);
   }, [])
+  const metaDescription = "Ravindra is a tech enthusiast and an explorer. He has worked as a Software Developer Intern at Kuvera and INDmoney in the past.";
 
   return (<>
     <Head>
+      <meta name="description" content={metaDescription}/>
+      <meta property="og:url" content="https://www.ravvis.codes/"/>
+      <meta property="og:image" content="https://www.ravvis.codes/images/opengraph.jpeg"/>
+      <meta property="og:description" content={metaDescription}/>
+      <meta property="og:title" content="Ravindra"/>
       <title>Ravindra</title>
       <link
         href="https://fonts.gstatic.com/s/muli/v22/7Auwp_0qiz-afTLGLQjUwkQ.woff2"
